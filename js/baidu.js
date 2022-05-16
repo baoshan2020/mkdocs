17d1d48f48c3ac46cf587829cfa4e0edc3b0cd3a
Ma BaoShan <398068229@qq.com>
2022-05-16 11:20:45 +0800

    2022/05/16 周一

---
+++ js/baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?62990005502ad0be641148268d644384";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
