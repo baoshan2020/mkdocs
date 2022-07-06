62c041f7f21369ab1f385a54e58e2ed8bf79eeb5
Ma BaoShan <398068229@qq.com>
2022-07-06 13:59:09 +0800

    2022/07/06 周三

--- daohang/wz.js
+++ daohang/wz.js
@@ -268,7 +268,7 @@ function content(jsoncountent) {
 
 //点击菜单执行函数
 function tab(param) {
-
+var param=param;
     // l(param);
     //清空颜色
     var liclear = document.getElementsByClassName('liactive')[0];
@@ -278,12 +278,12 @@ function tab(param) {
 
     //设置活动更新颜色
     var ul = document.getElementsByClassName('main-left')[0].getElementsByTagName('ul')[0];
-     var li = ul.document.getElementsByTagName('li');
-    //  var li=document.getElementsByTagName('li');
+    var li = ul.getElementsByTagName('li');
+
    // Cannot read properties of undefined (reading 'getElementsByTagName')
    // 缺少 document. 前缀
-
-
+alert(li);
+alert(li[param]);
 
     li[param].className = 'liactive';
 
